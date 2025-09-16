82babb95c1999f5621f6ee3e679ec0ba5289052a
abirrahman <abirrahman959@gmail.com>
2025-09-16 21:28:35 +0600

    added psuh notification

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,7 +15,7 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    namespace = "com.einfo.site"
+    namespace = "com.einfo.test"
     compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
@@ -31,7 +31,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.einfo.site"
+        applicationId = "com.einfo.test"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 24
--- android/app/src/main/kotlin/com/einfo/test/MainActivity.kt
+++ android/app/src/main/kotlin/com/einfo/test/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.einfo.site
+package com.einfo.test
 
 import io.flutter.embedding.android.FlutterActivity
 
